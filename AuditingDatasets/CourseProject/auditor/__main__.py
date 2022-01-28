d130edd17f330c0e3fa9a33aaed275ad0cada405
thirteenpylons <thirteenpylons@gmail.com>
2022-01-27 20:15:33 -0500

    o

--- AuditingDatasets/CourseProject/auditor/__main__.py
+++ AuditingDatasets/CourseProject/auditor/__main__.py
@@ -6,8 +6,6 @@ three lines of code (most of those imports).  It should call the execute() funct
 in module app.py, passing it the contents of sys.argv EXCEPT for the application name
 (so only the command line arguments AFTER 'python auditor').
 
-Author: Walker M. White (wmw2)
-Date:   June 7, 2019
 """
 import sys, app
 app.execute(sys.argv[1:])
